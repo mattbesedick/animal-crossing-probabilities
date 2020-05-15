478cf0a56d302b1c8f7eb9dd254a0c8702408af2
mattbesedick <mattbezy@gmail.com>
2020-05-14 20:38:30 -0500

    webpack configured and app running locally

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const pathLib = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: [
+		"@babel/polyfill", // enables async-await
+		"./client/index.js",
+	],
+	output: {
+		chunkFilename: "[name].bundle.js",
+		filename: "bundle.js",
+		path: pathLib.join(__dirname, "public"),
+		publicPath: "/",
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	devtool: "cheap-module-eval-source-map",
+	watchOptions: {
+		ignored: /node_modules/,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				include: pathLib.resolve(__dirname, "client"),
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				options: {
+					presets: ["@babel/preset-react"],
+				},
+			},
+		],
+	},
+};
