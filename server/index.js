3f4cd0f26a4381b5f390c71ec918a63a3d87612b
Matthew Besedick <mtbesedick@gmail.com>
2020-05-25 22:41:54 -0400

    test

--- server/index.js
+++ server/index.js
@@ -1,41 +1,42 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-const graphqlHttp = require("express-graphql");
-const mongoose = require("mongoose");
-const gqlSchema = require("./graphql/schema");
-const resolvers = require("./graphql/resolvers");
-const morgan = require("morgan");
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+const gqlSchema = require('./graphql/schema');
+const resolvers = require('./graphql/resolvers');
+const morgan = require('morgan');
 
 module.exports = app;
 
 const PORT = 4000;
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 //static file-serving middleware
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get("/*", (req, res) => {
-	res.sendFile(path.join(__dirname, "..", "public/index.html"));
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
 app.use(
-	"/api",
+	'/api',
 	graphqlHttp({
 		schema: gqlSchema,
 		rootValue: resolvers,
-		graphiql: true,
+		graphiql: true
 	})
 );
 
 mongoose
 	.connect(
-		`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@acnh-gihis.mongodb.net/acnh-stackathon?retryWrites=true&w=majority`
+		`mongodb+srv://${process.env.MONGO_USER}:${process.env
+			.MONGO_PASSWORD}@acnh-gihis.mongodb.net/acnh-stackathon?retryWrites=true&w=majority`
 	)
 	.then(() => {
-		app.listen(process.env.PORT || PORT, () => {
-			console.log("server is up at port 4000");
+		app.listen(process.env.PORT || 4000, () => {
+			console.log('server is up at port 4000');
 		});
 	})
 	.catch((err) => {
